0f6bc43ca02ccfce6017a8bd0a002f42b0b32b12
Julz Friedman <julz.friedman@uk.ibm.com>
2020-08-21 17:22:32 +0100

    Improve tests

--- max_test.go
+++ max_test.go
@@ -1,6 +1,7 @@
 package window_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/julz/window"
@@ -11,27 +12,27 @@ func TestWindowMax(t *testing.T) {
 		name      string
 		values    []float64
 		indexFunc func(int) int
-		expect    float64
+		expect    []float64
 	}{{
 		name:   "single value",
 		values: []float64{1},
-		expect: 1,
+		expect: []float64{1},
 	}, {
 		name:   "ascending values",
 		values: []float64{1, 2},
-		expect: 2,
+		expect: []float64{1, 2},
 	}, {
 		name:   "descending values",
 		values: []float64{2, 1},
-		expect: 2,
+		expect: []float64{2, 2},
 	}, {
 		name:   "up, down, up",
 		values: []float64{1, 2, 1},
-		expect: 2,
+		expect: []float64{1, 2, 2},
 	}, {
 		name:   "windowing out",
 		values: []float64{5, 6, 5, 5, 5, 5, 5},
-		expect: 5,
+		expect: []float64{5, 6, 6, 6, 6, 6, 5},
 	}, {
 		name:   "windowing out with gaps",
 		values: []float64{6, 5, 2, 1},
@@ -42,35 +43,35 @@ func TestWindowMax(t *testing.T) {
 
 			return i
 		},
-		expect: 2,
+		expect: []float64{6, 6, 6, 2},
 	}, {
 		name:   "windowing out 2",
 		values: []float64{5, 6, 5, 7, 5, 5, 1},
-		expect: 7,
+		expect: []float64{5, 6, 6, 7, 7, 7, 7},
 	}, {
 		name:   "windowing out 3",
 		values: []float64{5, 8, 5, 7, 5, 5},
-		expect: 8,
+		expect: []float64{5, 8, 8, 8, 8, 8},
 	}, {
 		name:   "windowing out 4",
 		values: []float64{5, 8, 5, 7, 5, 5, 1},
-		expect: 7,
+		expect: []float64{5, 8, 8, 8, 8, 8, 7},
 	}, {
 		name:   "windowing out 5",
 		values: []float64{5, 8, 5, 7, 5, 5, 1, 4, 4, 4},
-		expect: 5,
+		expect: []float64{5, 8, 8, 8, 8, 8, 7, 7, 5, 5},
 	}, {
 		name:   "windowing out 6",
 		values: []float64{5, 8, 5, 7, 5, 5, 1, 4, 4, 4, 4},
-		expect: 4,
+		expect: []float64{5, 8, 8, 8, 8, 8, 7, 7, 5, 5, 4},
 	}, {
 		name:   "windowing out 7",
 		values: []float64{5, 8, 5, 7, 5, 5, 1, 4, 4, 4, 4, 9},
-		expect: 9,
+		expect: []float64{5, 8, 8, 8, 8, 8, 7, 7, 5, 5, 4, 9},
 	}, {
 		name:   "windowing out 8",
 		values: []float64{5, 8, 5, 7, 5, 5, 1, 4, 4, 4, 4, 9, 3, 4, 2, 1, 0},
-		expect: 4,
+		expect: []float64{5, 8, 8, 8, 8, 8, 7, 7, 5, 5, 4, 9, 9, 9, 9, 9, 4},
 	}}
 
 	for _, tt := range tests {
@@ -82,11 +83,13 @@ func TestWindowMax(t *testing.T) {
 				indexFunc = tt.indexFunc
 			}
 
+			current := make([]float64, 0, len(tt.expect))
 			for i, v := range tt.values {
 				max.Record(indexFunc(i), v)
+				current = append(current, max.Current())
 			}
 
-			if got, want := max.Current(), tt.expect; got != want {
+			if got, want := current, tt.expect; !reflect.DeepEqual(got, want) {
 				t.Errorf("Current() = %f, expected %f", got, want)
 			}
 		})
